285fe3b353564a5c7ab331eb1bfeb3b10ae05efa
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2024-08-19 14:06:43 +0200

    set cpus-per-task=4

--- remote/cluster_timing.sh
+++ remote/cluster_timing.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=/gpfs/workdir/blampeyq/.jobs_outputs/%j
 #SBATCH --mem=256G
 #SBATCH --time=24:00:00
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=4
 #SBATCH --partition=cpu_long
 
 module purge
