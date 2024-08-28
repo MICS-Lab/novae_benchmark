a075756514ac17ac4363b3220f30eef545997e43
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2024-08-28 18:03:11 +0200

    use fusion_shm_big

--- remote/umaps.sh
+++ remote/umaps.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem=512G
 #SBATCH --time=7-00:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --partition=cpu_long
+#SBATCH --partition=fusion_shm_big
 
 module purge
 module load anaconda3/2022.10/gcc-11.2.0
