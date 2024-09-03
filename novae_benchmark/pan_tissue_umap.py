779ded192e58bc1d0d581b7947ccce9be6486825
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2024-09-03 16:56:04 +0200

    umap for all mouse

--- novae_benchmark/pan_tissue_umap.py
+++ novae_benchmark/pan_tissue_umap.py
@@ -40,20 +40,28 @@ def compute_and_save_umap(adata: AnnData, res_path: Path):
     adata.write_h5ad(res_path / f"adata_{adata.n_obs}_{run_ID}.h5ad")
 
     colors = []
-    for key in ["domain", "tissue", "technology", "novae_domains_15", "novae_domains_20", "novae_domains_25"]:
+    for key in [
+        "domain",
+        "tissue",
+        "technology",
+        "novae_domains_15",
+        "novae_domains_20",
+        "novae_domains_22",
+        "novae_domains_25",
+    ]:
         if key in adata.obs:
             colors.append(key)
 
     for color in colors:
         sc.pl.umap(adata, color=color, show=False, title=title_dict.get(color, color))
         sns.despine(offset=10, trim=True)
-        plt.savefig(res_path / f"umap_{adata.n_obs}_{color}.png", bbox_inches="tight", dpi=300)
+        plt.savefig(res_path / f"umap_{adata.n_obs}_{color}_{run_ID}.png", bbox_inches="tight", dpi=300)
 
 
 def main():
     # data_path = Path("/Users/quentinblampey/dev/novae/data/results/dry-wood-40")
     # res_path = Path("/Users/quentinblampey/dev/novae_benchmark/figures")
-    data_path = Path("/gpfs/workdir/blampeyq/novae/data/results/gallant-water-184")
+    data_path = Path("/gpfs/workdir/blampeyq/novae/data/results/still-surf-209")
     res_path = Path("/gpfs/workdir/blampeyq/novae_benchmark/figures")
 
     adata_full = sc.read_h5ad(data_path / "adata_conc.h5ad")
